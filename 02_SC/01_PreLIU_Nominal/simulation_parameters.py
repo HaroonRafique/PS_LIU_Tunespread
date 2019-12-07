bd9da5814e7c0e4323116bfe756e6d2994770e66
Haroon Rafique <haroon.rafique@protonmail.com>
2019-12-07 23:55:22 +0100

    02: simulation_parameters updated to proper table values

--- 02_SC/01_PreLIU_Nominal/simulation_parameters.py
+++ 02_SC/01_PreLIU_Nominal/simulation_parameters.py
@@ -11,31 +11,53 @@ parameters['tuney']						= '624'
 parameters['lattice_start'] 		= 'BWSV64'
 parameters['n_macroparticles']			= int(5E5)
 
-parameters['gamma']				= 2.49253731343
-parameters['intensity']			= 72.5E+10
-parameters['bunch_length']		= 140e-9
-parameters['blength']			= 140e-9
-parameters['epsn_x']			= 1E-6
-parameters['epsn_y']			= 1.2E-6
-parameters['dpp_rms']			= 8.7e-04
+# PS Injection 1.4 GeV
+if parameters['Machine'] is 'PreLIU':
+	parameters['gamma'] 	= 2.49038064
+	if parameters['Beam'] is 'Nominal':
+		parameters['intensity']			= 16.84E+11
+		parameters['epsn_x']			= 2.25E-6
+		parameters['epsn_y']			= 2.25E-6
+		parameters['bunch_length']		= 180e-9
+		parameters['blength']			= 180e-9
+		parameters['dpp_rms']			= 0.9E-03
+		parameters['rf_voltage']		= 0.0251 # 25.1 kV
+	elif parameters['Beam'] is 'BCMS':
+		parameters['intensity']			= 8.05E+11
+		parameters['epsn_x']			= 1.2E-6
+		parameters['epsn_y']			= 1.2E-6
+		parameters['bunch_length']		= 150e-9
+		parameters['blength']			= 150e-9
+		parameters['dpp_rms']			= 0.8E-03
+		parameters['rf_voltage']		= 0.0212 # 21.2 kV
+# PS Injection 2 GeV
+elif parameters['Machine'] is 'LIU':
+	parameters['gamma'] 	= 3.131540798
+	if parameters['Beam'] is 'Nominal':
+		parameters['intensity']			= 32.5E+11
+		parameters['epsn_x']			= 1.8E-6
+		parameters['epsn_y']			= 1.8E-6
+		parameters['bunch_length']		= 205e-9
+		parameters['blength']			= 205e-9
+		parameters['dpp_rms']			= 1.5E-03
+		parameters['rf_voltage']		= 0.0418 # 41.8 kV
+	elif parameters['Beam'] is 'BCMS':
+		parameters['intensity']			= 16.25E+11
+		parameters['epsn_x']			= 1.43E-6
+		parameters['epsn_y']			= 1.43E-6
+		parameters['bunch_length']		= 135e-9
+		parameters['blength']			= 135e-9
+		parameters['dpp_rms']			= 1.1E-03
+		parameters['rf_voltage']		= 0.03655 # 36.55 kV
+
+parameters['beta'] 		= np.sqrt(parameters['gamma']**2-1)/parameters['gamma']
 parameters['LongitudinalJohoParameter'] = 1.2
 parameters['LongitudinalCut'] 	= 2.4
 parameters['TransverseCut']		= 5
-parameters['rf_voltage']		= 0.0212942055190595723
 parameters['circumference']		= 2*np.pi*100
 parameters['phi_s']				= 0
 parameters['macrosize']			= parameters['intensity']/float(parameters['n_macroparticles'])
 parameters['tomo_file']			='Tomo_Files/PyORBIT_Tomo_file_'+parameters['Beam']+'_'+parameters['Machine']+'.mat'
-
-# PS Injection 1.4 GeV
-if parameters['Machine'] is 'PreLIU':
-	parameters['gamma'] 	= 2.49038064
-	parameters['beta'] 		= np.sqrt(parameters['gamma']**2-1)/parameters['gamma']
-
-# PS Injection 2 GeV
-elif parameters['Machine'] is 'LIU':
-	parameters['gamma'] 	= 3.131540798
-	parameters['beta'] 		= np.sqrt(parameters['gamma']**2-1)/parameters['gamma']
 
 c 						= 299792458
 parameters['sig_z'] 	= (parameters['beta'] * c * parameters['blength'])/4.
@@ -63,7 +85,7 @@ harmonic_factors = [1] # this times the base harmonic defines the RF harmonics (
 time = np.array([0,1,2])
 ones = np.ones_like(time)
 Ekin_GeV = 1.4*ones
-RF_voltage_MV = np.array([0.0212942055190595723*ones]).T # in MV
+RF_voltage_MV = np.array([parameters['rf_voltage']*ones]).T # in MV
 RF_phase = np.array([np.pi*ones]).T
 
 RFparameters = {
