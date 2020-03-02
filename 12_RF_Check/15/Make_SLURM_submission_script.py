3cdf5906fe9b5fae44be9f65166523453a8c4e15
Haroon Rafique <haroon.rafique@protonmail.com>
2020-03-02 10:25:14 +0100

    12: updated

--- 12_RF_Check/15/Make_SLURM_submission_script.py
+++ 12_RF_Check/15/Make_SLURM_submission_script.py
@@ -19,12 +19,12 @@ clean_all = True		# Clean simulation folder before running (False when resuming 
 # Must be chosen
 
 # ~ queue = 'inf-long', 'inf-short', 'batch-long', 'batch-short'
-queue = 'inf-long'
+queue = 'inf-short'
 
 n_nodes = 2 
 cwd = os.getcwd() # Get the present directory
 digits = int(cwd[-2:]) # Use last 2 digits to select our dp/p
-jobname = 'LIU10_'+str(digits)
+jobname = 'LIU12_'+str(digits)
 
 path_to_simulation = os.path.dirname(os.path.realpath(__file__)) # This directory
 
