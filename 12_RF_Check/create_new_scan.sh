e6282705c2a7decbe5679519a102d3e1bef30a4f
Haroon Rafique <haroon.rafique@protonmail.com>
2020-03-03 10:23:23 +0100

    12: updated with SC sims

--- 12_RF_Check/create_new_scan.sh
+++ 12_RF_Check/create_new_scan.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 folder_in=00_Master
-#folder_out=05
+folder_out=01
 
-#~ cp -r ${folder_in} ${folder_out}_01
-#~ cp -r ${folder_in} ${folder_out}_02
-#~ cp -r ${folder_in} ${folder_out}_03
-#~ cp -r ${folder_in} ${folder_out}_04
-#~ cp -r ${folder_in} ${folder_out}_05
-#~ cp -r ${folder_in} ${folder_out}_06
-#~ cp -r ${folder_in} ${folder_out}_07
-#~ cp -r ${folder_in} ${folder_out}_08
-#~ cp -r ${folder_in} ${folder_out}_09
-#~ cp -r ${folder_in} ${folder_out}_10
+cp -r ${folder_in} ${folder_out}_00
+cp -r ${folder_in} ${folder_out}_01
+cp -r ${folder_in} ${folder_out}_02
+cp -r ${folder_in} ${folder_out}_03
+cp -r ${folder_in} ${folder_out}_04
+cp -r ${folder_in} ${folder_out}_05
+cp -r ${folder_in} ${folder_out}_06
+cp -r ${folder_in} ${folder_out}_07
+cp -r ${folder_in} ${folder_out}_08
+cp -r ${folder_in} ${folder_out}_09
+cp -r ${folder_in} ${folder_out}_10
 #~ cp -r ${folder_in} ${folder_out}_11
 #~ cp -r ${folder_in} ${folder_out}_12
 #~ cp -r ${folder_in} ${folder_out}_13
@@ -22,15 +23,15 @@ folder_in=00_Master
 #~ cp -r ${folder_in} ${folder_out}_18
 #~ cp -r ${folder_in} ${folder_out}_19
 
-cp -r ${folder_in} 00
-cp -r ${folder_in} 01
-cp -r ${folder_in} 02
-cp -r ${folder_in} 03
-cp -r ${folder_in} 04
-cp -r ${folder_in} 05
-cp -r ${folder_in} 06
-cp -r ${folder_in} 07
-cp -r ${folder_in} 08
-cp -r ${folder_in} 09
-cp -r ${folder_in} 10
-cp -r ${folder_in} 11
+#~ cp -r ${folder_in} 00
+#~ cp -r ${folder_in} 01
+#~ cp -r ${folder_in} 02
+#~ cp -r ${folder_in} 03
+#~ cp -r ${folder_in} 04
+#~ cp -r ${folder_in} 05
+#~ cp -r ${folder_in} 06
+#~ cp -r ${folder_in} 07
+#~ cp -r ${folder_in} 08
+#~ cp -r ${folder_in} 09
+#~ cp -r ${folder_in} 10
+#~ cp -r ${folder_in} 11
