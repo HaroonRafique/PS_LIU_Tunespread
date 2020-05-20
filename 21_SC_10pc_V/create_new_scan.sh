ef89e58cba3783dac3c8a81bd0fc3dc247639d03
Haroon Rafique <haroon.rafique@protonmail.com>
2020-05-20 16:07:23 +0200

    21_SC_10pc_V/ added

---
+++ 21_SC_10pc_V/create_new_scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+folder_in=00_Master
+
+cp -r ${folder_in} Run2_BCMS
+cp -r ${folder_in} Run2_Standard
+cp -r ${folder_in} Run3_BCMS
+cp -r ${folder_in} Run3_Standard_2021
+cp -r ${folder_in} Run3_Standard_2022
+cp -r ${folder_in} Run3_Standard_2023
